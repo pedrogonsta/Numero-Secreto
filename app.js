32b76192b6f6822402784d4438666693fc7481b1
Pedro Goncalves <pedrogcost@gmail.com>
2025-03-05 15:41:19 -0300

    alteracao html

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ function textoInicial() {
 }
 
 let listaDeNumerosSorteados = [];
-let limiteNumeros = 50;
+let limiteNumeros = 40;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 textoInicial();
