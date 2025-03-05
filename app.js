4ab697b68559b6d99ffc4717270be63e98f66989
Pedro Goncalves <pedrogcost@gmail.com>
2025-03-05 15:13:59 -0300

    Deixar o jogo mais fácil

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ function textoInicial() {
 }
 
 let listaDeNumerosSorteados = [];
-let limiteNumeros = 100;
+let limiteNumeros = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 textoInicial();
